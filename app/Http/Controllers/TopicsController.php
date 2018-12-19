78503a8589210129cf2f757ad182dbdd57ec0bb4
chenjianeng <751855556@qq.com>
2018-12-19 12:25:27 +0000

    topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -45,6 +45,7 @@ class TopicsController extends Controller
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
+        $categories = Category::all();
 		return view('topics.create_and_edit', compact('topic'));
 	}
 
