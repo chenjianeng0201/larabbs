f22a1edeb69ee68d9807793fb3f97d458c2a537b
chenjianeng <751855556@qq.com>
2018-12-20 12:29:49 +0000

    reply

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
 	{
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 
         //
         \Carbon\Carbon::setLocale('zh');
