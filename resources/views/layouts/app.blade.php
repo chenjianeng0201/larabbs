9eb979fa9930023d54c0917fe5fb47be5d0c9d92
chenjianeng <751855556@qq.com>
2018-12-22 01:11:01 +0000

    Admin can manage setting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
-    <meta name="description" content="@yield('description', 'LaraBBS')" />
+    <title>@yield('title', 'LaraBBS') - {{ setting('site_name', 'Lara') }}</title>
+    <meta name="description" content="@yield('description', setting('seo_description', 'Lara 爱好者社区'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区'))" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
